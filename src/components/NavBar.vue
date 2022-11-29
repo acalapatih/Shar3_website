9a27e52651e315e2b3f34f2c6c7ae3f768c0395d
amirace <amiracalapati@gmail.com>
2022-11-29 09:58:33 +0700

    update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="main">
-        <nav class="flex items-center justify-between bg-[#7BCEA0] p-6">
-            <div class="imgNav flex items-center flex-shrink-0 ml-20">
+        <nav class="flex items-center justify-between bg-[#7BCEA0] p-6 px-20">
+            <div class="imgNav flex items-center flex-shrink-0">
                 <img src="@/assets/imgNav.png">
             </div>
             <div class="navbar flex lg:items-center lg:w-auto">
@@ -21,7 +21,7 @@
                 </div>
                 <div class="rightNav ml-8">
                     <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded-2xl text-white font-medium bg-[#2E567A] hover:border-transparent hover:text-[#2E567A] hover:bg-white mt-4 lg:mt-0">Donate Online</a>
-                    <img src="@/assets/search.png" class="inline-block ml-5">
+                    <!-- <img src="@/assets/search.png" class="inline-block ml-5"> -->
                 </div>
                 <div class="hamburger-menu ml-3 py-5" @click="menu()">
                     <img src="@/assets/menu.png" alt="">
@@ -48,20 +48,23 @@ export default {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@400;500;600;700&display=swap');
+ @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap');
 nav {
     font-family: 'Poppins', sans-serif;
 }
+
 .rightNav img {
     width: 25px;
     height: 25px;
     cursor: pointer;
 }
+
 .hamburger-menu img {
     width: 25px;
     height: 25px;
     cursor: pointer;
 }
+
 .hamburger-menu {
   display: none;
 }
@@ -71,6 +74,7 @@ nav {
         display: flex;
         flex-direction: row;
     }
+
     .leftNav {
         display: none;
         position: absolute;
@@ -82,32 +86,42 @@ nav {
         text-align: center;
         background-color: #fff;
     }
+
     .active-nav {
         display: block;
         z-index: 20;
     }
+
     .hamburger-menu {
         display: flex;
         align-items: center;
         display: block;
         z-index: 100;
     }
+
     .hamburger-menu img {
         cursor: pointer;
     }
 }
 @media only screen and (max-width: 500px) {
+    nav {
+        padding-left: 2rem;
+        padding-right: 2rem;
+    }
     .imgNav {
         width: 30%;
-        margin-left: 10px;
+        margin-left: 5px;
     }
+
     .rightNav {
         margin-left: 20px;
     }
+
     .rightNav a {
         font-size: x-small;
         padding: 5px;
     }
+
     .rightNav img, .hamburger-menu img {
         width: 15px;
         height: 15px;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,14 @@
 <template>
-  <NavBar />
-  <div class="main">
-
+<NavBar />
+<div class="main">
+  <div class="heroComp flex mx-10 my-48">
+    <h3 class="text-5xl font-semibold py-24">
+      Ayo berkembang bersama<br/>
+      <span class="text-[#1D8925]">Sahabat Remaja</span>
+    </h3>
+    <img src="@/assets/imgHero.png" class="inline-block ml-16">
   </div>
+</div>
 </template>
 
 <script>
@@ -23,5 +29,61 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap');
+ 
+.heroComp h3 {
+  font-family: 'Montserrat', sans-serif;
+}
 
+.heroComp img {
+  width: 420px;
+  height: 350px;
+}
+
+@media only screen and (max-width: 1030px) {
+  .heroComp {
+    margin-top: 8rem;
+    margin-bottom: 8rem;
+  }
+
+  .heroComp h3{
+    font-size: 26px;
+  }
+
+  .heroComp img{
+    width: 350px;
+    height: 290px;
+  }
+}
+@media only screen and (max-width: 720px) {
+  .heroComp {
+    margin-top: 5rem;
+    margin-bottom: 5rem;
+  }
+
+  .heroComp h3{
+    font-size: 18px;
+  }
+
+  .heroComp img{
+    width: 270px;
+    height: 225px;
+  }
+}
+@media only screen and (max-width: 500px) {
+  .heroComp {
+    margin: 3rem 2rem;
+  }
+
+  .heroComp h3{
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    font-size: 10px;
+  }
+  
+  .heroComp img{
+    width: 120px;
+    height: 100px;
+  }
+}
 </style>
